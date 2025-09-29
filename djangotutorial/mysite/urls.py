9a7f135431a15ee9030faae634b3d85989a06f61
rohan-g0re <rohang.atwork@gmail.com>
2025-09-29 12:40:22 -0400

    deployed successfully

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from .views import home
 
 urlpatterns = [
+    path("", home, name="home"),
     path("polls/", include("polls.urls", namespace="polls")),
     path("admin/", admin.site.urls),
 ]
